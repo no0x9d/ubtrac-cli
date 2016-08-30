ac2fd741abb5360627ed844c93348e8dd95e0556
Christoph Hoffmann <christoph.hoffmann@nestedloop.de>
2016-08-31 01:33:22 +0200

    update styling for focused elements and focus handling in todo-details

--- src/command-handler/todo/widgets/todo-details.js
+++ src/command-handler/todo/widgets/todo-details.js
@@ -24,31 +24,56 @@ module.exports = function Details() {
     cols: 1,
   });
 
-  box.title = grid.set(0, 0, 1, 1, blessed.Box, {label: 'Title'});
-  box.description = grid.set(1, 0, 5, 1, contrib.markdown, {label: 'Description'});
-  box.workorder = grid.set(6, 0, 1, 1, blessed.Box, {label: 'Workorder'});
-  box.project = grid.set(7, 0, 1, 1, blessed.Box, {label: 'Project'});
-  box.state = grid.set(8, 0, 1, 1, blessed.Box, {label: 'State'});
+  box.title = grid.set(0, 0, 1, 1, blessed.Box, {label: 'Title', clickable: true, keyable: true});
+  box.description = grid.set(1, 0, 5, 1, contrib.markdown, {label: 'Description', clickable: true, keyable: true});
+  box.workorder = grid.set(6, 0, 1, 1, blessed.Box, {label: 'Workorder', clickable: true, keyable: true});
+  box.project = grid.set(7, 0, 1, 1, blessed.Box, {label: 'Project', clickable: true, keyable: true});
+  box.state = grid.set(8, 0, 1, 1, blessed.Box, {label: 'State', clickable: true, keyable: true});
   box.worklog = grid.set(9, 0, 3, 1, blessed.List, {label: 'Worklog'});
 
   function hideWithEscape(ch, key) {
     if (!box.hidden && key.name === 'escape') {
       box.hide();
+      box.screen.rewindFocus();
       box.screen.render();
       return false;
     }
   }
 
+  function setupKeyListener(el, ch, key) {
+    if (key.name === 'down') {
+      box.screen.focusNext();
+    } else if (key.name === 'up') {
+      box.screen.focusPrev();
+    }
+  }
+
   box.on('show', ()=> {
+    box.title.focus();
     box.onScreenEvent('keypress', hideWithEscape);
+    box.on('element keypress', setupKeyListener)
   });
   box.on('hide', ()=> {
     box.removeScreenEvent('keypress', hideWithEscape);
+    box.removeListener('element keypress', setupKeyListener)
+  });
+
+  //set green border
+  box.on('element focus', function(el) {
+    if (el.border) el.style.border.fg = 'green';
+    box.screen.render();
+  });
+
+  //reset border
+  box.on('element blur', function(el) {
+    if (el.border) el.style.border.fg = 'default';
+    box.screen.render();
   });
 
   box.setItem = function(item) {
     this.title.setContent(item.title);
     this.description.setMarkdown(item.description || '');
+    this.description.set('markdown', item.description);
     this.workorder.setContent(item.workorder);
     this.project.setContent(item.project);
     this.state.setContent(item.state);
