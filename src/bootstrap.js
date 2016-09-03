12d2f1e0b3d8da23aafae125acbe5520366760af
Christoph Hoffmann <christoph.hoffmann@nestedloop.de>
2016-09-03 23:56:54 +0200

    implement Jira integration for task import
    also implement a config system, where jira host and credentials can be stored

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -5,12 +5,26 @@ const osenv = require('osenv');
 const path = require('path');
 const mkdirp = require('mkdirp');
 
-var dbPath = path.join(osenv.home(), '.ubtrac', 'db');
-mkdirp.sync(dbPath);
+const ubtracHome = path.join(osenv.home(), '.ubtrac');
+mkdirp.sync(ubtracHome);
 
+//config
+const configPath = path.join(ubtracHome, 'config.json');
+
+let config;
+try {
+  config = require(configPath);
+} catch (e) {
+  config = {}
+}
+
+//database
+const dbPath = path.join(ubtracHome, 'db');
+mkdirp.sync(dbPath);
 const db = new Db(dbPath, {});
 
 const ubtrac = Ubtrac(db);
 
 module.exports.db = db;
 module.exports.ubtrac = ubtrac;
+module.exports.config = config;
