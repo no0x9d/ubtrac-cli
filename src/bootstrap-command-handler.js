ed43f9ceff0cb49de513d47328bbe400598dfc8f
Christoph Hoffmann <christoph.hoffmann@nestedloop.de>
2016-09-10 00:34:30 +0200

    remove console.log for params

--- src/bootstrap-command-handler.js
+++ src/bootstrap-command-handler.js
@@ -1,8 +1,6 @@
 module.exports = bootstrapCommandHandler;
 
 function bootstrapCommandHandler(argv, handler) {
-
-  console.log(argv, handler);
   const context = require('./bootstrap');
   return handler(argv, context);
 }
