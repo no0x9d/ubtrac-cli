0d6ea72fca3e9997feb578d4ebc094ce87612e16
Christoph Hoffmann <christoph.hoffmann@nestedloop.de>
2016-09-12 00:05:50 +0200

    implement options to work list for narrowing down which work log items to show.
    These options are compatible with the aptrac cli api.

--- src/command-handler/work/list.js
+++ src/command-handler/work/list.js
@@ -1,8 +1,39 @@
+'use strict';
+const moment = require('moment');
+
 module.exports = listWorkHandler;
 
 function listWorkHandler(argv, context) {
+
+  var from, to;
+  // handle time based queries
+  // handle option flags and set 'from' & 'to'
+
+  if(argv.from || argv.to){
+    from = argv.from && moment(argv.from) || moment().startOf('day');
+    to = argv.to && moment(argv.to) || moment().endOf('day');
+  } else {
+    var unitOfTime = 'day';
+    if (argv.week) {
+      unitOfTime = 'week';
+    }
+    if (argv.month) {
+      unitOfTime = 'month';
+    }
+    if (argv.year) {
+      unitOfTime = 'year';
+    }
+    from = moment().startOf(unitOfTime);
+    to = moment().endOf(unitOfTime);
+
+    if (argv.last) {
+      from = from.subtract(1, unitOfTime);
+      to = to.subtract(1, unitOfTime);
+    }
+  }
+
   const workRepository = context.ubtrac.workLogRepository;
-  workRepository.findAll()
+  workRepository.findByTime(from.toDate(), to.toDate())
     .then(w => console.log(w))
     .catch(err => console.log(err.message))
 }
--- src/commands/work_cmds/list.js
+++ src/commands/work_cmds/list.js
@@ -1,7 +1,46 @@
 'use strict';
 exports.command = 'list';
 exports.desc = 'lists all work items';
-exports.builder = { };
+exports.builder = {
+  from: {
+    alias: 'f',
+    describe: 'time from which to query (default: today 0:00 am)',
+    type: 'string',
+    coerce: (input) => {
+      if (input == undefined) return undefined;
+      return require('../../cli-util/date-time-parser')(input).toDate()
+    }
+  },
+  to: {
+    alias: 't',
+    describe: 'time to which to query (default: today 11:59 pm)',
+    type: 'string',
+    coerce: (input) => {
+      if (input == undefined) return undefined;
+      return require('../../cli-util/date-time-parser')(input).toDate()
+    }
+  },
+  week: {
+    alias: 'w',
+    describe: 'show all entries of current week. is partially overwritten by -f and -t',
+    type: 'boolean'
+  },
+  month: {
+    alias: 'm',
+    describe: 'show all entries of current month. is partially overwritten by -f and -t',
+    type: 'boolean'
+  },
+  year: {
+    alias: 'y',
+    describe: 'show all entries of current year. is partially overwritten by -f and -t',
+    type: 'boolean'
+  },
+  last: {
+    alias: 'l',
+    describe: 'shows the last unit of the chosen time frame, e.g. -lw for last week. use together with -w -m -y or without -f or -t to get day.',
+    type: 'boolean'
+  }
+};
 exports.handler = function(argv) {
   const bootstrapCommandHandler = require('../../bootstrap-command-handler');
   bootstrapCommandHandler(argv, require('../../command-handler/work/list'))
